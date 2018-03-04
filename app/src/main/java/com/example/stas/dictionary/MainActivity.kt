01e3627ec204b65322f8a5ed42fa5d0748a4621d
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-04 20:28:27 +0300

    Добавил страницу Recent

--- app/src/main/java/com/example/stas/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/stas/dictionary/MainActivity.kt
@@ -8,14 +8,9 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-  
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.tab1favorites.view.*
 
 class MainActivity : AppCompatActivity() {
 
