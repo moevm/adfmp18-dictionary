3d8a9ef3e95e5c109c0d78cc78fda657078b5012
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-04 21:37:11 +0300

    Добавил ListView на таб favorites

--- app/src/main/java/com/example/stas/dictionary/Tab1Favorites.kt
+++ app/src/main/java/com/example/stas/dictionary/Tab1Favorites.kt
@@ -8,10 +8,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.tab1favorites.*
+
 
 class Tab1Favorites: Fragment(){
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView: View? = inflater?.inflate(R.layout.tab1favorites, container, false)
+
+        //TODO вытягивать данные из БД
+        val favorites = arrayOf("Продукты", "Кухня", "Поездка")
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, favorites)
+        val listView: ListView = activity.favoritesListView
+        listView.adapter = adapter
+
+        
+
         return rootView
     }
 }
