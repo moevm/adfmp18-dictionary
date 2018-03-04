747198a7b732ca10a3ae8241909c055c6d85dc43
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-04 23:29:10 +0300

    Добавил контекстное меню для ListView в Tab1Favorites

--- app/src/main/java/com/example/stas/dictionary/Tab1Favorites.kt
+++ app/src/main/java/com/example/stas/dictionary/Tab1Favorites.kt
@@ -5,15 +5,18 @@ package com.example.stas.dictionary
  */
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.tab1favorites.*
 
 
 class Tab1Favorites: Fragment(){
+    //TODO вытягивать данные из БД
     private var favorites = arrayOf("Продукты", "Кухня", "Поездка")
     private var adapter: ArrayAdapter<String> ?= null
     private var listView: ListView ?= null
@@ -21,11 +24,24 @@ class Tab1Favorites: Fragment(){
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView: View? = inflater?.inflate(R.layout.tab1favorites, container, false)
 
-        //TODO вытягивать данные из БД
+
         adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, favorites)
         listView = rootView?.findViewById<ListView>(R.id.favoritesListView)
         listView?.adapter = adapter
+        registerForContextMenu(listView)
+
+
+        listView?.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(context, "Position Clicked:"+" "+position,Toast.LENGTH_SHORT).show()
+        }
 
         return rootView
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menu!!.setHeaderTitle("Select the action")
+        menu!!.add(0, v!!.id, 0, "Delete")
+        menu!!.add(0, v!!.id, 0, "Like")
     }
 }
