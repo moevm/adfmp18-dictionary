b535f0ab405d4971c9428fecda0dd330a78c4f7d
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-04 15:41:26 +0300

    Убрал Main tool bar

--- app/src/main/java/com/example/stas/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/stas/dictionary/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
