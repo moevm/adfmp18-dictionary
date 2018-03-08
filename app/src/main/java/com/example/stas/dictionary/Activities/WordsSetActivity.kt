63a7dcaeafa7ff204541ea170f37a5b9b4c75953
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-08 08:46:04 +0300

    Добавил удаление слова из набора

--- app/src/main/java/com/example/stas/dictionary/Activities/WordsSetActivity.kt
+++ app/src/main/java/com/example/stas/dictionary/Activities/WordsSetActivity.kt
@@ -1,7 +1,11 @@
 package com.example.stas.dictionary.Activities
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import com.example.stas.dictionary.Data.WordsSet
 import com.example.stas.dictionary.R
@@ -13,29 +17,48 @@ class WordsSetActivity : AppCompatActivity() {
 
     private var listView: ListView?= null
     private var ATTRIBUTE_WORD_TEXT = "text"
-    private var simpleAdapter: SimpleAdapter ?= null
+    private var simpleAdapter: MySimpleAdapter ?= null
     private var from = arrayOf(ATTRIBUTE_WORD_TEXT)
     private var to = IntArray(1, {R.id.tvWordSetItem})
+    private var wordsSet : WordsSet = WordsSet("defalt", arrayOf("default word - default translate"))
+    private var data : ArrayList<Map<String, Any>> = ArrayList<Map<String, Any>>(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_words_set)
 
-        var wordsSet = intent.getParcelableExtra<WordsSet>("test")
+        wordsSet = intent.getParcelableExtra<WordsSet>("test")
         tvWordsSetName.text = wordsSet.name
 
-        var data: ArrayList<Map<String, Any>> = ArrayList<Map<String, Any>>(wordsSet.words.size)
+        data = ArrayList<Map<String, Any>>(wordsSet.words.size)
         var m: Map<String, Any>
-        for(word in wordsSet.words){
+        for (word in wordsSet.words) {
             m = HashMap<String, Any>()
             m.put(ATTRIBUTE_WORD_TEXT, word)
             data.add(m)
         }
 
-        simpleAdapter = SimpleAdapter(this, data, R.layout.words_set_item, from, to)
+        simpleAdapter = MySimpleAdapter(this, data, R.layout.words_set_item, from, to)
 
         listView = lvWordsSet
         listView?.adapter = simpleAdapter
+
+    }
+
+    inner class MySimpleAdapter: SimpleAdapter{
+        constructor(context: Context?, data: List<out Map<String, *>>?,
+                    resource: Int, from: Array<out String>?, to: IntArray?) : super(context, data, resource, from, to)
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var view = super.getView(position, convertView, parent)
+            var imageView = view.findViewById<ImageView>(R.id.tvWordSetItemDelete)
+            imageView.setTag(position)
+            imageView.setOnClickListener{
+                data.removeAt(position)
+                notifyDataSetChanged()
+            }
+            return view
+        }
 
 
     }
