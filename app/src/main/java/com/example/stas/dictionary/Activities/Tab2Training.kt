589d69af8a084850a598c38a062157b72646fc10
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-12 02:13:51 +0300

    Добавил отображение всплывающео окна для выбора наборов для тренировки

--- app/src/main/java/com/example/stas/dictionary/Activities/Tab2Training.kt
+++ app/src/main/java/com/example/stas/dictionary/Activities/Tab2Training.kt
@@ -3,16 +3,47 @@ package com.example.stas.dictionary.Activities
 /**
  * Created by stas on 04.03.18.
  */
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
 import android.support.v4.app.Fragment
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.*
+import com.example.stas.dictionary.Data.WordsSet
 import com.example.stas.dictionary.R
 
 class Tab2Training : Fragment(){
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView: View? = inflater?.inflate(R.layout.tab2training, container, false)
+
+        var btnTraining = rootView?.findViewById<Button>(R.id.btnTraining)
+
+        btnTraining?.setOnClickListener({
+            val display : Display = activity.windowManager.defaultDisplay
+            val size = Point()
+            display.getRealSize(size)
+            val width = size.x
+
+            val view = layoutInflater.inflate(R.layout.popup_training, null)
+            val popupWindow = PopupWindow(view)
+
+            popupWindow.width = (width * 0.85).toInt()
+            popupWindow.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            popupWindow.isFocusable = true
+            // If you need the PopupWindow to dismiss when when touched outside
+            popupWindow.setBackgroundDrawable(ColorDrawable())
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+            popupWindow.showAsDropDown(view)
+
+
+
+
+        })
+
         return rootView
     }
+
+
 }
