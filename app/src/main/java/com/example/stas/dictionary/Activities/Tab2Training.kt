d4de844635a348efda90cd15d52f582f86f83de2
StanislavTalanov <rationalegoism1470@gmail.com>
2018-03-12 02:19:23 +0300

    Добавил адаптер для ListView наборов

--- app/src/main/java/com/example/stas/dictionary/Activities/Tab2Training.kt
+++ app/src/main/java/com/example/stas/dictionary/Activities/Tab2Training.kt
@@ -11,8 +11,12 @@ import android.view.*
 import android.widget.*
 import com.example.stas.dictionary.Data.WordsSet
 import com.example.stas.dictionary.R
+import kotlinx.android.synthetic.main.activity_new_set.*
 
 class Tab2Training : Fragment(){
+
+    private var allSets = arrayOf("set1", "set2", "set3",
+            "set2", "set3", "set2", "set3","set2", "set3","set2", "set3")
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var rootView: View? = inflater?.inflate(R.layout.tab2training, container, false)
@@ -35,6 +39,12 @@ class Tab2Training : Fragment(){
             popupWindow.setBackgroundDrawable(ColorDrawable())
             popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
 
+            var listView = view.findViewById<ListView>(R.id.lvTraining)
+            var adapter = ArrayAdapter(context, android.R.layout.simple_list_item_multiple_choice, allSets)
+
+            listView?.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+            listView?.adapter = adapter
+            
             popupWindow.showAsDropDown(view)
 
 
