c6769afbff71f4c623909d70bf0f44883c682ab6
Paul Osmolovsky <paul.osmolovsky@gmail.com>
2018-03-26 23:36:04 +0300

    Попытки подчинить progressbar Исправлен текст

--- app/src/main/java/com/example/stas/dictionary/Activities/Training.kt
+++ app/src/main/java/com/example/stas/dictionary/Activities/Training.kt
@@ -12,26 +12,47 @@ class Training : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trainig)
 
+        var currQ = 0
+        val allQ = 4
+
         var btn1 = btnAnswer1
         var btn2 = btnAnswer2
         var btn3 = btnAnswer3
         var btn4 = btnAnswer4
         var btnStop = btnStop
 
+        fun refreshProc() {
+            textViewProcess.text = currQ.toString() + " / " + allQ.toString()
+            progressBar2.incrementProgressBy(Int.MAX_VALUE / allQ)
+            progressBar2.progress = 100 * (currQ / allQ)
+            if (currQ == allQ) {
+                Toast.makeText(this, "Great job!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        refreshProc()
+
         btn1?.setOnClickListener({
             Toast.makeText(this, "answer 1", Toast.LENGTH_SHORT).show()
+            currQ++
+            refreshProc()
         })
 
         btn2?.setOnClickListener({
             Toast.makeText(this, "answer 2", Toast.LENGTH_SHORT).show()
+            currQ++
+            refreshProc()
         })
 
         btn3?.setOnClickListener({
             Toast.makeText(this, "answer 3", Toast.LENGTH_SHORT).show()
+            currQ++
+            refreshProc()
         })
 
         btn4?.setOnClickListener({
             Toast.makeText(this, "answer 4", Toast.LENGTH_SHORT).show()
+            currQ++
+            refreshProc()
         })
 
         btnStop?.setOnClickListener({
